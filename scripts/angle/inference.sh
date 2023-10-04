d928ed885eda39886d9d480614c24829af6fdc59
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2023-10-04 18:53:10 +0100

    Change specific paths to generic ones

--- scripts/angle/inference.sh
+++ scripts/angle/inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 box_confidence=0.25
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_6epochs_5e-3/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_6epochs_5e-3/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -10,7 +10,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e8_1e-3" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_8epochs_5e-3/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_8epochs_5e-3/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -19,7 +19,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e8_5e-3" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_8epochs_1e-2/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_8epochs_1e-2/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -28,7 +28,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e8_1e-2" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_4epochs_1e-3/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_4epochs_1e-3/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -37,7 +37,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e4_1e-3" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_4epochs_5e-3/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_4epochs_5e-3/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -46,7 +46,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e4_5e-3" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_4epochs_1e-2/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_4epochs_1e-2/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -57,7 +57,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
 
 
 ## BEST RUNS
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_4epochs_1e-2/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_4epochs_1e-2/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -66,7 +66,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle-e4_1e-2" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_best-seed42/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_best-seed42/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -75,7 +75,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle_best-seed42" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_best-seed821/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_best-seed821/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
@@ -84,7 +84,7 @@ python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --name "custom_angle_best-seed821" \
     --save-txt
 
-pretrained_path="/home/samuh/Desktop/results_angle/custom_angle_best-seed1765/weights/best.pt"
+pretrained_path="./results_angle/custom_angle_best-seed1765/weights/best.pt"
 python3 ./adapt/detect_SeaDronesSeeDataset.py \
     --weights $pretrained_path \
     --conf $box_confidence \
